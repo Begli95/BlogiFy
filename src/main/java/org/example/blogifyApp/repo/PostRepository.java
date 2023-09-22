714794d1dcab18c7566529970282f68a96b71f15
Begli95 <saparov-begli@mail.ru>
2023-09-23 01:19:16 +0400

    step7

--- src/main/java/org/example/blogifyApp/repo/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.blogifyApp.repo;
-
-import org.example.blogifyApp.models.Post;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PostRepository extends CrudRepository<Post,Long> {
-
-}
--- src/main/java/org/example/blogifyApp/repo/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.blogifyApp.repo;
-
-import org.example.blogifyApp.models.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User,Long> {
-    User findByName(String name);
-}
